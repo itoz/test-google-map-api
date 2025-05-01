32c33b07a8d3b4d290e3baeac74a69807ea04f27
itoz <romatica@gmail.com>
2025-05-01 20:09:45 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/test-googlemap-api/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/test-google-map-api/' : '/',
   build:{outDir :'docs'}
 })
