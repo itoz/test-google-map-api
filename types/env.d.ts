b3d9007bdcefaa881cc3b8b080fa6e33d97a979a
itoz <romatica@gmail.com>
2025-05-01 20:02:53 +0900

    fix

---
+++ types/env.d.ts
@@ -0,0 +1,3 @@
+// src/env.d.ts または types/env.d.ts に保存
+
+/// <reference types="vite/client" />
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/test-googlemap-api/',
+  base: process.env.NODE_ENV === 'production' ? '/test-googlemap-api/' : '/',
   build:{outDir :'docs'}
 })
